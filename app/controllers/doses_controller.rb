9f624fc49faff8db92dba700cb19a203d31d6322
Daniel Mera de Agustin <daniel.meradeagustin@gmail.com>
2018-11-22 17:29:07 +0100

    creating cocktails and adding doses

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,2 +1,24 @@
 class DosesController < ApplicationController
+  def new
+    @dose = Dose.new
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @ingredients = Ingredient.all
+  end
+
+  def create
+    @dose = Dose.new(dose_params)
+    @dose.cocktail_id = params[:cocktail_id]
+    raise
+    if @dose.save
+      redirect_to cocktail_path(params[:cocktail_id])
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def dose_params
+    params.require(:dose).permit(:description, :ingredient_id)
+  end
 end
